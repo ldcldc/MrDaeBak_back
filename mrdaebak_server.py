cd76efaa9a138c19a82d94eae3a44a5a6ef3d696
Tanney-102 <dhxodms89@gmail.com>
2020-11-17 01:43:04 +0900

    init

---
+++ mrdaebak_server.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+from db_model.mysqldb_conn import conn_mysqldb
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello() :
+    return '<h1>Hello!!</h1>'
+
+
+@app.route('/db_test', methods=['get']) 
+def db_test():
+    db = conn_mysqldb()
+    db_cursor = db.cursor()
+    sql = "select * from user_info"
+    db_cursor.execute(sql)
+    user_row = db_cursor.fetchone()
+
+    user = {
+        'user_id' : user_row[0],
+        'user_name' : user_row[1],
+        'user_pw' : user_row[2],
+        'address' : user_row[3],
+        'ordered' : user_row[4],
+        'class' : user_row[5]
+    }
+
+    return jsonify(user)
+
+
+
+app.run(host="0.0.0.0", port="8080", debug=True)
