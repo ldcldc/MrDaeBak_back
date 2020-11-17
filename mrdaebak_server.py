f748493edac9798d203d16b484670daedea30fd1
Tanney-102 <dhxodms89@gmail.com>
2020-11-17 22:12:58 +0900

    solve

--- mrdaebak_server.py
+++ mrdaebak_server.py
@@ -8,25 +8,25 @@ def index() :
     return '<h1>Hello!!</h1>'
 
 
-# @app.route('/db_test', methods=['get']) 
-# def db_test():
-#     db = conn_mysqldb()
-#     db_cursor = db.cursor()
-#     sql = "select * from user_info"
-#     db_cursor.execute(sql)
-#     user_row = db_cursor.fetchone()
+@app.route('/db_test', methods=['get']) 
+def db_test():
+    db = conn_mysqldb()
+    db_cursor = db.cursor()
+    sql = "select * from user_info"
+    db_cursor.execute(sql)
+    user_row = db_cursor.fetchone()
 
-#     user = {
-#         'user_id' : user_row[0],
-#         'user_name' : user_row[1],
-#         'user_pw' : user_row[2],
-#         'address' : user_row[3],
-#         'ordered' : user_row[4],
-#         'class' : user_row[5]
-#     }
+    user = {
+        'user_id' : user_row[0],
+        'user_name' : user_row[1],
+        'user_pw' : user_row[2],
+        'address' : user_row[3],
+        'ordered' : user_row[4],
+        'class' : user_row[5]
+    }
 
-#     return jsonify(user)
+    return jsonify(user)
 
 
-
-app.run()
+if __name__ == '__main__':
+    app.run()
