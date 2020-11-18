3625aecad60a7306b06112d618e5d61c99cfd2d7
Tanney-102 <dhxodms89@gmail.com>
2020-11-18 20:34:25 +0900

    register bp test_module

--- mrdaebak_server.py
+++ mrdaebak_server.py
@@ -1,31 +1,13 @@
 from flask import Flask, jsonify
 from db_model.mysqldb_conn import conn_mysqldb
+from router.test_module import test_module
 
 app = Flask(__name__)
+app.register_blueprint(test_module)
 
 @app.route('/')
 def index() :
-    return '<h1>Hello!!</h1>'
-
-
-@app.route('/db_test', methods=['get']) 
-def db_test():
-    db = conn_mysqldb()
-    db_cursor = db.cursor()
-    sql = "select * from user_info"
-    db_cursor.execute(sql)
-    user_row = db_cursor.fetchone()
-
-    user = {
-        'user_id' : user_row[0],
-        'user_name' : user_row[1],
-        'user_pw' : user_row[2],
-        'address' : user_row[3],
-        'ordered' : user_row[4],
-        'class' : user_row[5]
-    }
-
-    return jsonify(user)
+    return '<h1>Mr DaeBak API Server!</h1>'
 
 
 if __name__ == '__main__':
---
+++ router/test_module.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, jsonify
+from db_model.mysqldb_conn import conn_mysqldb
+
+test_module = Blueprint('test_module', __name__)
+
+@test_module.route('/db_test')
+def db_test():
+    db = conn_mysqldb()
+    db_cursor = db.cursor()
+    sql = "select * from user_info"
+    db_cursor.execute(sql)
+    user_row = db_cursor.fetchone()
+
+    user = {
+        'user_id' : user_row[0],
+        'user_name' : user_row[1],
+        'user_pw' : user_row[2],
+        'address' : user_row[3],
+        'ordered' : user_row[4],
+        'class' : user_row[5]
+    }
+
+    return jsonify(user)
+
+@test_module.route('/get_m_test/<id>')
+def get_m_test(id):
+    return '<h1>%s</h1>' % id
