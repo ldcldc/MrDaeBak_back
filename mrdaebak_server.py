de8e291d55607d0ef097febc91f4c7d11245f9a3
Tanney-102 <dhxodms89@gmail.com>
2020-11-19 22:00:26 +0900

    cors

--- mrdaebak_server.py
+++ mrdaebak_server.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
+from flask-cors import CORS
 from pakages.db_model.mysqldb_conn import conn_mysqldb
 from pakages.router.test_module import test_module
 from pakages.router.SignupController import signup_controller
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "1111"
+cors = CORS(app, resources={r"/api/*": {"origins": "https://mrdaebak.netlify.app"}})
+
 app.register_blueprint(test_module)
 app.register_blueprint(signup_controller)
 
