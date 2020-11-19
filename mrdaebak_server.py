dae9e69aa9744b90fc3fde0575ceccc9255e30ba
Tanney-102 <dhxodms89@gmail.com>
2020-11-19 22:18:18 +0900

    .

--- mrdaebak_server.py
+++ mrdaebak_server.py
@@ -5,8 +5,8 @@ from pakages.router.test_module import test_module
 from pakages.router.SignupController import signup_controller
 
 app = Flask(__name__)
+CORS(app)
 app.config["SECRET_KEY"] = "1111"
-CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.register_blueprint(test_module)
 app.register_blueprint(signup_controller)
